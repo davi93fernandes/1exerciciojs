4e0786aba0f5573ae695e6f6dc644229e1b5225f
davi93fernandes <davi.a.fernandes@hotmail.com>
2023-03-09 01:45:43 -0300

    exerc.pronto

--- calc.js
+++ calc.js
@@ -5,43 +5,20 @@ function calcular() {
     const n3 = document.getElementById("num3").value
     //processamento
     let resultado = Number(n1) + Number(n2) + Number(n3)
-    // console.log(resultado)
-    //alert(resultado)
     //saída
     document.getElementById("resultado").innerHTML = resultado
 }
-//document.addEventListener("load")
 
 function calc() {
+    //capturando no html, mudando typo de dado e selecionando o atributo valor
+    const n4 =  Number (document.getElementById("num4").value)
+    const n5 = Number (document.getElementById("num5").value)
+    const n6 = Number (document.getElementById("num6").value)
     
-    const n4 = document.getElementById("num4")
-    const n5 = document.getElementById("num5")
-    const n6 = document.getElementById("num6")
+    //process.
+   let total = n4 + n5 + n6
+    //
+    const resultadoDois = document.getElementById("resultadoDois")
+    console.log(resultadoDois.innerHTML = `Total da soma: ${total}`)
 
-    const val1 = Number(n4.value)
-    const val2 = Number(n5.value)
-    const val3 = Number(n6.value)
-
-    total = val1 + val2 + val3
-
-     const resultadoDois = document.getElementById("resultadoDois")
-    console.log (resultadoDois.innerHTML = `Total da soma: ${total}`)
-
-}
-function somar_ex() {
-    // PEGANDO OS VALORES DOS INPUTS
-    let total = 0
-    const valor1 = document.getElementById("val1")
-    const valor2 = document.getElementById("val2")
-    const valor3 = document.getElementById("val3")
-
-    // PASSANDO PARA NUMERO - TENDO CERTEZA QUE SÃO NUMEROS E NÃO TEXTO PARA CONCATENAR
-    const value1 = Number(valor1.value)
-    const value2 = Number(valor2.value)
-    const value3 = Number(valor3.value)
-
-    // PROCESSAMENTO - CALCULO
-    total = value1 + value2 + value3
-    const resultado = document.getElementById("total_ex")
-    resultado.innerText = "Total da Soma: " + total
 }
